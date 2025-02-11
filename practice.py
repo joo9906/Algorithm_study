0eaaa60e9b875fa37c67a65d7cde90cb684c0f6a
왕주영 노트북 <happyboyjoo@gmail.com>
2025-02-11 23:26:57 +0900

    2월 11일 마무리

--- practice.py
+++ practice.py
@@ -1,6 +1,9 @@
-a = '()(((()())(())()))(())'
-b = []
-try:
-    b.pop()
-except IndexError:
-    print(0)
+T = int(input())
+
+def check():
+    full, part = map(str, input().split())
+    check = full.replace(part, '*', len(full))
+    return len(check)
+
+for i in range(1, T+1):
+    print(f'#{i} {check()}')
