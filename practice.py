572117c4eb1971d38bacefc0a2be185fd63dae3b
왕주영 노트북 <happyboyjoo@gmail.com>
2025-02-13 00:40:44 +0900

    2월 12일 밤

--- practice.py
+++ practice.py
@@ -1,17 +1,29 @@
 T = int(input())
 
-def shuffle():
-    n = int(input())
-    stage = [list(map(int, input().split())) for _ in range(n)]
-    
-    delta = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    
-    for x in range(n):
-        for y in range(n):
-            for i, j in delta:
-                
+def stone():
+    n, m = map(int, input().split())
+    arr = list(map(int, input().split()))
+    result = []
+    for _ in range(m):
+        i, j = map(int, input().split())
+        a = arr[0:i-1]
+        a = a.reverse()
+        b = arr[i:n]
+        for k in range(j-1):
+            if a[k] == b[k] == 1:
+                a[k] = b[k] = 0
+
+            elif a[k] == b[k] == 0:
+                a[k] = b[k] = 1
+
+            else :
+                a[k], b[k] = b[k], a[k]
+    result.extend(a)
+    result.extend(b)
 
     return result
 
-for i in range(1,T+1):
-    print(f'#{i} {shuffle()}')
+
+
+for i in range(1, T+1):
+    print(f'#{i} {stone()}')
--- test.py
+++
@@ -1,4 +0,0 @@
-DNA = 'CCTGGATTG'
-S = 9
-P = 8
-print(DNA[:8] - DNA[0])
