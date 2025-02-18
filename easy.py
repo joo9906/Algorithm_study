2e63bb9d5370cfe0a17588f84bc13a7f70d4d89d
왕주영 노트북 <happyboyjoo@gmail.com>
2025-02-19 00:04:12 +0900

    모니터 힘들다

--- easy.py
+++ easy.py
@@ -1,3 +1,16 @@
-n = 5
-arr = [0] * n
-print(arr)
+n = int(input())
+arr = set(map(int, input().split()))
+m = int(input())
+brr = list(map(int, input().split()))
+
+result = []
+
+for num in brr:
+    if num in arr:
+        result.append(1)
+    else:
+        result.append(0)
+
+
+
+print(*result)
--- practice.py
+++ practice.py
@@ -1,20 +1,46 @@
+T = int(input())
+stack = []
+check = '+-*/'
+last = []
+def forth():
+    arr = list(map(str, input().split()))
 
+    for i in arr:
+        if arr[i] not in check:
+            arr[i] = int(arr[i])
+    
+    for i in range(len(arr)):
+        if arr[i] == '.':
+            stack.append('.')
+            if len(arr) == 2:
+                return arr[i-1]
 
-def sugar():
-    n = int(input())
-    sugar_bag = [5, 3]
-    cnt = 0
+        if arr[i] == '+' and arr[i-1] not in check :
+            result = arr[i-2] + arr[i-1]
+            stack.append(result)
+            arr.pop(i-1)
+            arr.pop(i-1)
 
-    for i in sugar_bag:
-        if n >= i:
-            cnt += n//i
-            n = n % i
+        if arr[i] == '-' and arr[i-1] not in check:
+            result = arr[i-2] - arr[i-1]
+            stack.append(result)
+            arr.pop(i-1)
+            arr.pop(i-1)
+            stack.append('-')
+            
 
+        if arr[i] == '*' and arr[i-1] not in check:
+            result = arr[i-2] * arr[i-1]
+            stack.append(result)
+            arr.pop(i-1)
+            arr.pop(i-1)
+            stack.append('*')
 
+        if arr[i] == '/' and arr[i-1] not in check :
+            result = arr[i-2] / arr[i-1]
+            stack.append(result)
+            arr.pop(i-1)
+            arr.pop(i-1)
+            stack.append('/')
 
-    if n != 0:
-        return print(-1)
-    else:
-        return print(cnt)
-
-sugar()
+            
