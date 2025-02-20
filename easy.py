7b542720f25af5b55e905726caa8a202a5536ff4
jooyoung <happyboyjoo@naver.com>
2025-02-20 23:52:45 +0900

    graph 쉬운거 끝. 깃허브에 붙이기

--- easy.py
+++ easy.py
@@ -1,18 +1,37 @@
-n, m = map(int, input().split())
-stack = []
-arr = [i for i in range(1, n+1)]
-visited = [False] * (n)
+from collections import deque
+import sys
+sys.stdin = open('input.in', 'r')
 
-def nm(cnt):
-    if len(stack) == m:
-        print(*stack)
-        return 
+T = int(input())
+
+def find(start, under): 
+    while inj[start]:
+        node = inj[start].popleft()
+        under.append(node)
+        visited[node] = True
+        find(node, under)
+    return under
+
+for nums in range(1, T+1):
+    people = int(input())
+    visited = [False] * (people)
+    start = 0
+    under = []
+    boss = 0
+    inj = [deque() for _ in range(people)]
+
+    arr = [list(map(int, input().split())) for _ in range(people)] 
     
-    for i in range(cnt, n+1):
-        if i not in stack:
-            stack.append(i)
-            nm(i+1)
-            stack.pop()
+    for i in range(people): # 누가 누구랑 연결되어있는지 arr을 순회하며 찾음
+        for j in range(people):
+            if arr[i][j] == 1:
+                inj[i].append(j)
+                
+    for k in range(len(inj)):
+            if 0 in inj[k]:
+                boss = k
+                
+    
 
-nm(1)
-print(arr)
+    print(f'#{nums} boss:{boss} / under:', end = '')
+    print(*find(start, under))
