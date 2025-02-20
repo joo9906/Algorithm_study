a4276fdcbf9e521fc1f8e49feb4cd531f72dfef6
Sehwan Park <geistsfust21@gamil.com>
2025-02-21 08:56:57 +0900

    파일정리

--- Algorithm/Queue/graph1.py
+++ Algorithm/Queue/graph1.py
@@ -70,3 +70,70 @@ for i in range(1, T+1):
                 print(*q)
                 q.pop()
         q.pop()
+# ------------------------------------------------------------------------
+# 바이러스 - DFS
+from collections import deque
+ 
+T = int(input())
+ 
+for nums in range(1, T+1):
+    com_num = int(input())
+    line = int(input())
+    visited = [False] * (com_num+1)
+    start = 1
+    cnt = 0
+ 
+    inj = [deque() for _ in range(com_num+1)]
+ 
+    for i in range(line): # 컴퓨터들이 몇 번 컴퓨터랑 연결되어 있는지 확인
+        n, m = map(int, input().split())
+        inj[n].append(m)
+        inj[m].append(n)
+         
+    def virus(start): # 1번과 연결된 컴퓨터면 visited를 True로 바꾸는 함수(재귀)
+        while inj[start]:
+            node = inj[start].popleft()
+            visited[node] = True
+            virus(node)
+ 
+    virus(start)
+    result = visited.count(True)
+         
+ 
+    print(f'#{nums} {result-1}')
+
+# ----------------------------------------------------------------------------
+# 상사와 부하
+from collections import deque
+ 
+T = int(input())
+ 
+def find(start, under): 
+    while inj[start]:
+        node = inj[start].popleft()
+        under.append(node)
+        visited[node] = True
+        find(node, under)
+    return under
+ 
+for nums in range(1, T+1):
+    people = int(input())
+    visited = [False] * (people)
+    start = 0
+    under = []
+    boss = 0
+    inj = [deque() for _ in range(people)]
+ 
+    arr = [list(map(int, input().split())) for _ in range(people)] 
+     
+    for i in range(people): # 누가 누구랑 연결되어있는지 arr을 순회하며 찾음
+        for j in range(people):
+            if arr[i][j] == 1:
+                inj[i].append(j)
+                 
+    for k in range(len(inj)):
+            if 0 in inj[k]:
+                boss = k
+ 
+    print(f'#{nums} boss:{boss} / under:', end = '')
+    print(*find(start, under))
