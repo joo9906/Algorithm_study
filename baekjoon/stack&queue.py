8107b5d28c28e4d4e55d6e0e3070c0f85e09d793
jooyoung <happyboyjoo@naver.com>
2025-02-27 00:41:28 +0900

    12시 41분

--- baekjoon/stack&queue.py
+++ baekjoon/stack&queue.py
@@ -12,3 +12,67 @@ while len(stack) > 1:
     stack.append(b)
 
 print(*stack)
+
+# --------------------------------------------------------
+# 1158 요세푸스
+n,k = map(int,input().split())
+
+
+# ----------------------------------------------------------
+# 9935 문자폭탄
+def strings():
+    arr = input().rstrip()
+    bomb = input().rstrip()
+    result = []
+    bl = len(bomb)
+    
+    for i in arr:
+        result.append(i)
+        if len(result) >= bl and result[-bl:] == list(bomb):
+            del result[-bl:]
+    
+    if not result:
+        return 'FRULA'
+    else:
+        return ''.join(result)
+        
+print(strings())
+
+# -----------------------------------------------------------------
+# 17413 단어 뒤집기 2
+def word():
+    words = input().strip()
+    result = []
+    save = deque()
+    check = 0
+    
+
+    for i in words:
+        if i == ' ':
+            result.extend(save)
+            result.append(' ')
+            save = deque()
+            continue
+            
+        if i == '<':
+            check = 1
+            if save:
+                result.extend(save)
+                save = deque()
+        
+        if i == '>':
+            result.append(i)
+            check = 0
+            continue
+        
+        if check == 1:
+            result.append(i)
+        elif check == 0:
+            save.appendleft(i)
+            
+    if save:
+        result.extend(save)
+    
+    return print(''.join(result))
+
+word()
--- easy.py
+++ easy.py
@@ -1,5 +1,38 @@
 from collections import deque
 
-dice = deque([0] * 7) # 주사위 6면(0번째는 세지 않으면 됨)
-comp = deque(list(map(int, input().split())))
-print(comp)
+def word():
+    words = input().strip()
+    result = []
+    save = deque()
+    check = 0
+    
+
+    for i in words:
+        if i == ' ':
+            result.extend(save)
+            result.append(' ')
+            save = deque()
+            continue
+            
+        if i == '<':
+            check = 1
+            if save:
+                result.extend(save)
+                save = deque()
+        
+        if i == '>':
+            result.append(i)
+            check = 0
+            continue
+        
+        if check == 1:
+            result.append(i)
+        elif check == 0:
+            save.appendleft(i)
+            
+    if save:
+        result.extend(save)
+    
+    return print(''.join(result))
+
+word()
