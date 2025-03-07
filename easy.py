64c31d455e1cdb2ae31781428f3dab27ed5f2199
Sehwan Park <geistsfust21@gamil.com>
2025-03-07 11:55:09 +0900

    7일 11시 55분

--- easy.py
+++ easy.py
@@ -1,20 +1,23 @@
+import sys
+sys.stdin = open("input.in", 'r')
 
-def make_tree(start, end, i):
-    if start == end:
-        print(i)
-        binary_tree[i] = arr[i]
-        return
+def dfs(node, visited):
+    visited |= (1 << node) # node의 수만큼 shift를 해서 어디를 방문 했는지 표현하는 것.
+    print(node, visited)
+    for next_node in range(N):
+        if graph[node][next_node] == 0:
+            continue
 
-    mid = (start+end) // 2
-    make_tree(start, mid, i*2)
-    make_tree(mid+1, end, i*2+1)
+        if visited & (1 << next_node): #visited의 자리와 다음 노드의 자리가 1로 같으면 방문한거임
+            continue
 
+        dfs(next_node, visited)
 
 T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    graph = [list(map(int, input().split())) for _ in range(N)]
 
-for k in range(1, T+1):
-    n = int(input())
-    arr = [i for i in range(1, n+1)]
-    binary_tree = [0] * (len(arr) + 2)
-    make_tree(0, len(arr)-1, 1)
-    print(binary_tree)
+    visited = 0 # 초기 방문 상태
+    dfs(0, visited)
+
