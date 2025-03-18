42b47136126927becd7aeb48645368ce362b777b
왕주영 노트북 <happyboyjoo@gmail.com>
2025-03-18 20:53:15 +0900

    카페 끝

--- practice.py
+++ practice.py
@@ -1,49 +1,12 @@
-import sys, heapq, copy
-from collections import deque
-#sys.stdin = open('input.txt', 'r')
+import sys
+sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 
-class Answer:
-    def __init__(self, n ,arr, target):
-        self.n = n
-        self.arr = arr
-        self.tree = {i:[] for i in range(self.n)}
-        self.build()
-        self.remove(target)
-        self.cnt = 0
-        self.recur(0)
 
-    def build(self):
-        for k in range(self.n):
-            if arr[k] == -1:
-                continue
-            self.tree[self.arr[k]].append(k)
+arr = []
+while True:
+    start, end = map(int, input().split())
+    if start == 0 and end == 0:
+        break
 
-    def remove(self, target):
-        if target in self.tree:
-            del self.tree[target]
-
-        for p in self.tree:
-            if target in self.tree[p]:
-                self.tree[p].remove(target)
-
-    def recur(self, node):
-        if 0 not in self.tree.keys():
-            return
-
-        if not self.tree[node]:
-            self.cnt += 1
-            return
-
-        for ch in self.tree[node]:
-            self.recur(ch)
-
-    def result(self):
-        return self.cnt
-
-n = int(input().strip())
-arr = list(map(int, input().strip().split()))
-target = int(input().strip())
-
-solve = Answer(n, arr, target)
-print(solve.cnt)
+    arr.append((start,end))
