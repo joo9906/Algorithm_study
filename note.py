51bd558169d649e1e7bad76a8fc7df66c2569ead
jooyoung <happyboyjoo@naver.com>
2025-03-20 00:16:54 +0900

    'home'

--- note.py
+++
@@ -1,3 +0,0 @@
-if date == 0321:
-    remove(KKR)
-print(서울 6반의 세상이 무너지는 날)
--- practice.py
+++ practice.py
@@ -1,12 +1,5 @@
 import sys
-from collections import deque
-sys.stdin = open('input.txt', 'r')
-#input = sys.stdin.readline
-
-
-
-n = int(input())
-dp = [0] * (n+1)
-
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
+#from collections import deque
+#sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
