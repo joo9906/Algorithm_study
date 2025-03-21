64617a47bfcd92047232be745354ff390b007dd5
Sehwan Park <geistsfust21@gamil.com>
2025-03-21 17:49:43 +0900

    'ssafy'

--- practice.py
+++ practice.py
@@ -3,52 +3,27 @@ from collections import deque
 sys.stdin = open('input.txt', 'r')
 #input = sys.stdin.readline
 
-class Train:
-    def __init__(self, n, arr):
+class Snowman:
+    def __init__(self, n, m, arr):
         self.n = n
-        self.graph = arr
-        self.lis = self.dijkstra(0)
-        self.result = self.lis[-1]
+        self.m = m
+        self.arr = arr
+        for i in range(n):
+            for j in range(m):
+                if arr[i][j] == 2:
+                    start_x, start_y = i, j
+                elif arr[i][j] == 3:
+                    target_x, target_y = i, j
+        self.delta = [None, (0, 1), (1, 0), (0, -1), (-1, 0)] # 위나 아래는 짝수
+        #self.route(start_x, start_y, target_x, target_y)
 
-    def dijkstra(self, start_node):
-        q = [(0, start_node)]
-        distance = [float('inf')] * self.n
-        distance[start_node] = 0
 
-        while q:
-            weight, node = heapq.heappop(q)
+    def route(self, sx, sy, tx, ty):
+        q = [(0, sx, sy)]
 
-            if node == self.n-1:
-                break
-
-            if distance[node] < weight: # 현재 뽑은 가중치보다 이전에 설정한 가중치가 더 작으면 넘어감
-                continue
-
-            for info in graph[node]:
-                next_node = info[0]
-                next_dist = info[1]
-
-                ndist = weight + next_dist
-
-                if distance[next_node] > ndist:
-                    distance[next_node] = ndist
-                    heapq.heappush(q, (ndist, next_node))
-
-        return distance
 
 T = int(input())
 for tc in range(1, T+1):
-    n, t = map(int, input().split())
-    graph = [[] for _ in range(n)]
-
-    for _ in range(t):
-        a, b, w = map(int, input().split())
-        graph[a].append((b, w))
-
-    solve = Train(n, graph)
-
-    if solve.result < float('inf'):
-        print(f'#{tc} {solve.result}')
-    else:
-        print(f'#{tc} impossible')
-
+    n, m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    solve = Snowman(n, m, arr)
