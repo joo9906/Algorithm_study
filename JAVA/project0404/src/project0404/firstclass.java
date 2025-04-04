d147452c64c6e63d8b752926e536dc1eab28432f
wangjooyoung <happyboyjoo@naver.com>
2025-04-04 09:04:41 +0900

    1

---
+++ JAVA/project0404/src/project0404/firstclass.java
@@ -0,0 +1,11 @@
+package project0404;
+
+import java.util.Arrays;
+
+public class firstclass {
+	public static void main(String[] args) {
+		int[][] arr1 = {{1, 2, 3}, {2, 3, 4}};
+		
+		System.out.println(Arrays.toString(arr1));
+	}
+}
