0f2480053e1261bd9878cc0834a6da6254126a27
wangjooyoung <happyboyjoo@naver.com>
2025-04-11 16:07:16 +0900

    1

---
+++ JAVA/project0404/src/project0404/Main.java
@@ -0,0 +1,9 @@
+package project0404;
+
+class Main {
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; ++i) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ JAVA/project0404/src/project0404/aaaa.java
@@ -0,0 +1,5 @@
+package project0404;
+
+public class aaaa {
+
+}
