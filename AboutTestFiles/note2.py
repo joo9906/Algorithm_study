ac2cc40f61b9be9684983dfb9ef80636e4c2dd35
Wangjooyoung <happyboyjoo@naver.com>
2025-07-29 08:32:37 +0900

    1

--- AboutTestFiles/note2.py
+++ AboutTestFiles/note2.py
@@ -1,1 +1,29 @@
-print([0] * 4)
+n = int(input())
+q = []
+what = []
+
+for _ in range(n):
+    data = tuple(input().split())
+    what.append(data)
+print(what)
+for i in what:
+    if len(i) == 2:
+        q.append(i[1])
+    else:
+        do = i[0]
+        if do == "front":
+            print(q[0])
+        elif do == "back":
+            print(q[-1])
+        elif do == "size":
+            print(len(q))
+        elif do == "empty":
+            if not q:
+                print(1)
+            else:
+                print(0)
+        elif do == "pop":
+            if q:
+                print(q.pop())
+            else:
+                print(-1)
