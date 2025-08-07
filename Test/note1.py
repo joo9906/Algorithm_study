8748fe59aad8dc78ed7abe4b6c095a04573f7226
왕주영 노트북 <happyboyjoo@gmail.com>
2025-08-07 23:59:56 +0900

    콩쥐야 살려줘

--- Test/note1.py
+++ Test/note1.py
@@ -1,33 +1,38 @@
-def solution(Board):
-  n = len(Board)
-  m = len(Board[0])
-  max_num = 0
-  visited = [[False] * m for _ in range(n)] # 방문한곳 재사용 X
+from collections import deque
 
-  delta = [(0, 1), (1, 0), (0, -1), (-1, 0)] # 2차원 배열이므로 델타로 탐색
+def solve(nodes, start, target):
+    answer = 1001
+    q = deque()
+    for val in nodes[start]:
+        check = set()
+        check.add(val)
+        q.append((check, val, 0))
 
-  def dfs(x, y, arr, depth):
-      nonlocal max_num
+    while q:
+        checks, now, cnt = q.popleft()
 
-      #총 4자리수를 만들었으면 합쳐서 정수형으로 다시 변환(0을 넣으려고 할 떄 정수로 넣으면 안되니까 문자형으로 넣었음)
-      if depth == 4: 
-          num = int(''.join(arr))
-          max_num = max(max_num, num)
-          return
+        if target in checks:
+            answer = min(answer, cnt)
+            continue
+        
+        if nodes[now]:
+            for next in nodes[now]:
+                if next not in checks:
+                    checks.add(next)
+                    q.append((checks, next, cnt + 1))
 
-      #델타 돌면서 범위 내에 있고 방문 X면 arr에 추가하고 다음꺼 찾으러 감
-      for dx, dy in delta:
-          nx, ny = x + dx, y + dy
-          if 0 <= nx < n and 0 < ny < m and visited[nx][ny] == False:
-              visited[nx][ny] = True
-              dfs(nx, ny, arr + [str(Board[nx][ny])], depth + 1)
-              visited[nx][ny] = False
+    if answer < 1001:
+        return answer
+    else:
+        return 0
+    
+T = int(input())
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    nodes = {i:[] for i in range(1, V+1)}
+    for _ in range(E):
+        start, end = map(int, input().split())
+        nodes[start].append(end)
+    S, G = map(int, input().split())
 
-  # Board의 모든 좌표를 시작점으로 잡아서 돌려보고 만들 수 있는 가장 큰 수를 반환
-  for i in range(n):
-      for j in range(m):
-          visited[i][j] = True
-          dfs(i, j, [str(Board[i][j])], 1)
-          visited[i][j] = False
-
-  return max_num
+    print(f'#{tc} {solve(nodes, S, G)}')
